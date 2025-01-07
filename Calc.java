23970373b4daf5872c966dd71bc52b7ab25b2682
Metha-42 <metha.siva4236@gmail.com>
2025-01-07 23:22:37 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,22 @@
+package pack;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+		
+		
+                //Primitive DataType Size 	
+		        System.out.println("Size of byte: " + Byte.BYTES + " bytes");
+		        System.out.println("Size of short: " + Short.BYTES + " bytes");
+		        System.out.println("Size of int: " + Integer.BYTES + " bytes");
+		        System.out.println("Size of long: " + Long.BYTES + " bytes");
+		        System.out.println("Size of float: " + Float.BYTES + " bytes");
+		        System.out.println("Size of double: " + Double.BYTES + " bytes");
+		        System.out.println("Size of char: " + Character.BYTES + " bytes");
+		        System.out.println("Size of boolean: " + 1 + " bit (typically)");
+		    		}
+
+	
+	
+}
