5701bd396968a0a985c6b6b2f823d61769da1ba1
Metha-42 <metha.siva4236@gmail.com>
2025-01-07 20:32:43 +0530

    Add files via upload

---
+++ Adding.java
@@ -0,0 +1,56 @@
+package pack;
+
+public class Adding {
+
+	public static void main(String[] args) {
+		
+		String stdName =" Metha";
+		int rollNo= 42;
+		String stdClass= "Bsc.cs";
+		String stdSec= "B";
+		int mark1=50;
+		int mark2=60;
+		int mark3=70;
+		int mark4=80;
+		int mark5=100;
+		
+		//Average
+		double avg=(mark1+mark2+mark3+mark4+mark5)/5.0;
+		
+		System.out.println("Student Name Is"+" "+stdName);
+		
+		System.out.println("Student Roll No Is"+" "+rollNo);
+		
+		System.out.println("Student Class Is"+" "+stdClass);
+		
+		System.out.println("Student Section Is"+" "+stdSec);
+		
+		System.out.println("Student Mark1 Is"+" "+mark1);
+
+		System.out.println("Student Mark2 Is"+" "+mark2);
+	
+		System.out.println("Student Mark3 Is"+" "+mark3);
+		
+		System.out.println("Student Mark4 Is"+" "+mark4);
+		
+		System.out.println("Student Mark5 Is"+" "+mark5);
+		
+		System.out.println("Student Average  Is"+" "+avg);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+}
